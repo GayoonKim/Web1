0cc049ee8db63b913a9882e8b2d90cb62fc759b5
GayoonKim <gayunkim.1@gmail.com>
2022-09-25 01:47:25 +0900

    함수의 사용

--- index.php
+++ index.php
@@ -1,48 +1,63 @@
+<?php
+function print_title()
+{
+    if (isset($_GET['id'])) {
+        echo $_GET['id'];
+    } else {
+        echo 'Welcome';
+    }
+}
+
+function print_description()
+{
+    if (isset($_GET['id'])) {
+        echo file_get_contents("data/" . $_GET['id']);
+    } else {
+        echo "Hello, PHP";
+    }
+}
+
+function print_list()
+{
+    $filelist = scandir('./data');
+    $i = 0;
+    while ($i < count($filelist)) {
+        if ($filelist[$i] != '.') {
+            if ($filelist[$i] != '..') {
+                echo "<li> <a href=\"index.php?id=$filelist[$i]\"> $filelist[$i] </a></li>\n";
+            }
+        }
+        $i += 1;
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title> <?php print_title(); ?></title>
 </head>
+
 <body>
     <h1><a href="index.php">WEB</a></h1>
 
     <ol>
         <?php
-            $filelist = scandir('./data');
-            $i = 0;
-            while ($i < count($filelist)) {
-                if($filelist[$i] != '.') {
-                    if($filelist[$i] != '..') {
-                        echo "<li> <a href=\"index.php?id=$filelist[$i]\"> $filelist[$i] </a></li>\n";
-                    }
-                }
-                $i += 1;
-            }
+        print_list();
         ?>
     </ol>
 
-    <h2> 
-        <?php 
-        
-            if(isset($_GET['id'])) {
-                echo $_GET['id'];
-            } else {
-                echo 'Welcome'; 
-            }
+    <h2> <?php print_title(); ?> </h2>
 
-        ?> 
-    </h2>
- 
-    <?php 
+    <?php
 
-        if(isset($_GET['id'])) {
-            echo file_get_contents("data/".$_GET['id']);
-        } else {
-            echo "Hello, PHP";  
-        }
-
+    print_description();
+    
     ?>
 
 </body>
+
 </html>
