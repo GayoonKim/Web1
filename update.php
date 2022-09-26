9bdcd3139aeda73d0da9426b1d3571b996987368
GayoonKim <gayunkim.1@gmail.com>
2022-09-26 16:58:29 +0900

    PHP 글 수정 기능 추가

---
+++ update.php
@@ -0,0 +1,83 @@
+<?php
+function print_title()
+{
+    if (isset($_GET['id'])) {
+        echo $_GET['id'];
+    } else {
+        echo 'Welcome';
+    }
+}
+
+function print_description()
+{
+    if (isset($_GET['id'])) {
+        echo file_get_contents("data/" . $_GET['id']);
+    } else {
+        echo "Hello, PHP";
+    }
+}
+
+function print_list()
+{
+    $filelist = scandir('./data');
+    $i = 0;
+    while ($i < count($filelist)) {
+        if ($filelist[$i] != '.') {
+            if ($filelist[$i] != '..') {
+                echo "<li> <a href=\"index.php?id=$filelist[$i]\"> $filelist[$i] </a></li>\n";
+            }
+        }
+        $i += 1;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title> <?php print_title(); ?></title>
+</head>
+
+<body>
+    <h1><a href="index.php">WEB</a></h1>
+
+    <ol>
+        <?php
+        print_list();
+        ?>
+    </ol>
+
+    <a href="create.php"> Create </a>
+
+    <?php if (isset($_GET['id'])) { ?>
+        <a href="update.php?id=<?= $_GET['id'] ?>"> Update </a>
+    <?php } ?>
+
+    <h2>
+        <form action="update_process.php" method="POST">
+            <input type="hidden" name="old_title" value="<?= $_GET['id']?>">
+            <p>
+
+                <input type="text" placeholder="Title" name="title" value="<?php print_title(); ?>">
+
+            </p>
+
+            <p>
+
+                <textarea name="description" placeholder="Description"><?php print_description(); ?></textarea>
+
+            </p>
+
+            <p>
+
+                <input type="submit">
+
+            </p>
+    </h2>
+
+</body>
+
+</html>
