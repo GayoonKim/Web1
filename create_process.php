1d6fb733d7fd03a7bdb8f55d3c19f049764a0f43
GayoonKim <gayunkim.1@gmail.com>
2022-09-29 15:15:38 +0900

    MySQL과 연동

--- create_process.php
+++ create_process.php
@@ -1,7 +1,32 @@
 <?php
 
-file_put_contents('data/'.$_POST['title'], $_POST['description']);
+$conn = mysqli_connect('localhost', 'root', 'asdf1234', 'opentutorials');
 
-header('Location: /index.php?id='.$_POST['title']);
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+$sql = "
+    INSERT INTO topic
+    (title, description, created)
+    VALUES(
+        
+            '{$title}',
+            '{$description}',
+            NOW()
+
+        )
+
+";
+
+if ( !$result = mysqli_query($conn, $sql) )
+{
+    printf(mysqli_error($conn));
+}
+else
+{
+    echo '성공했습니다. <a href="index.php">돌아가기</a>';
+}
+
+mysqli_close($conn);
 
 ?>
